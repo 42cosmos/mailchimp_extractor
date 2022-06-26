646fed5746f391df75c8daea912e688fe3718349
Eunbin Park <cosmos42@icloud.com>
2022-06-27 00:30:15 +0900

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
 
+NAME = "extract-mailchimp-data"
+VERSION = "0.0.1"
+
+REQUIRES = [
+    "python-dotenv==0.20.0",
+    "mailchimp-marketing"
+]
 
 setup(
-    name             = 'MailChimp Data Extractor by folderID',
-    version          = '0.0.1',
-    description      = 'You can extract MailChimp data only by folder ID',
-    long_description = open('README.md').read(),
-    author           = 'Eunbin Park',
-    author_email     = 'parkeb417@gmail.com',
-    install_requires = ['requests'],
-    packages         = find_packages(exclude = ['docs', 'example']),
-    keywords         = ['bot', 'study', 'api'],
-    python_requires  = '>=3',
-    zip_safe=False,
-    classifiers      = [
-        'Programming Language :: Python :: 3.6',
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
-    python_requires='>=3.6'
+    name=NAME,
+    version=VERSION,
+    description="Extract MailChimp data only by folder ID",
+    long_description=open("README.md").read(),
+    author="Eunbin Park",
+    author_email="parkeb417@gmail.com",
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    keywords=["data", "inflab", "api", "mailchimp"],
+    python_requires=">=3.6",
+    include_package_data=True
 )
